41c06e46c69eab9e00eaebadf32db1c9d784da74
shahzebs <shahzeb.syed96@gmail.com>
2019-12-16 12:32:45 +0100

    Remove unecessary forward slash in sed substitution

--- .scripts/dmenu_configs/dmenu_configs.sh
+++ .scripts/dmenu_configs/dmenu_configs.sh
@@ -2,7 +2,7 @@
 
 filename=$(awk "/$(cat $HOME/.scripts/dmenu_configs/config_files.txt | awk '{print $1}' | dmenu)/ {print \$2}" $HOME/.scripts/dmenu_configs/config_files.txt)
 
-filedir=$(echo $filename | sed 's|\(.*\)\/.*|\1\/|')
+filedir=$(echo $filename | sed 's|\(.*\)\/.*|\1|')
 
 if [[ "$filename" != *$'\n'* ]]; then
     gnome-terminal --working-directory=$filedir -- /bin/sh -c "vim $filename; $SHELL"
