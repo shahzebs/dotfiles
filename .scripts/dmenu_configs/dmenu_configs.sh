24f6fe0e591e2771ec0a86f73533c755ca8741e8
shahzebs <shahzeb.syed96@gmail.com>
2019-12-19 20:00:56 +0100

    Update config file prompt code

--- .scripts/dmenu_configs/dmenu_configs.sh
+++ .scripts/dmenu_configs/dmenu_configs.sh
@@ -4,8 +4,8 @@ pathtoconfiglist=$HOME/.scripts/dmenu_configs/config_files.txt
 
 filename=$(awk "/$(cat $pathtoconfiglist | awk '{print $1}' | dmenu)/ {print \$2}" $pathtoconfiglist)
 
-filedir=$(echo $filename | sed 's|\(.*\)\/.*|\1|')
+# filedir=$(echo $filename | sed 's|\(.*\)\/.*|\1|')
 
 if [[ $filename ]] && [[ "$filename" != *$'\n'* ]] ; then
-    gnome-terminal --working-directory=$filedir -- /bin/sh -c "vim $filename; $SHELL"
+    st -e vim "$filename"
 fi
