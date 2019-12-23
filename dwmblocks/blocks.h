03c83fc3e1df4560b83415923026dd540cf0c1b8
shahzebs <shahzeb.syed96@gmail.com>
2019-12-23 05:26:22 +0100

    Add up/down wifi usage and music playback to status bar

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {" ", "$HOME/.scripts/statusbar/bluetooth", 10, 6},
+    {" ", "$HOME/.scripts/statusbar/playback", 15, 7},
+    {"", "$HOME/.scripts/statusbar/bluetooth", 10, 6},
     {"", "$HOME/.scripts/statusbar/dropbx", 10, 5},
     {"", "$HOME/.scripts/statusbar/cpu_temp", 5, 4},
     {"", "$HOME/.scripts/statusbar/memory", 5, 3},
-    {"", "$HOME/.scripts/statusbar/wifi", 15, 2},
+    {"", "$HOME/.scripts/statusbar/wifi", 5, 2},
     {"", "$HOME/.scripts/statusbar/volume", 0, 1},
 	{"", "$HOME/.scripts/statusbar/date", 30, 0},
 
