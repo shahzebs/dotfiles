c17941820e5352cb573466c8ea87ef85b8a1cc35
shahzebs <shahzeb.syed96@gmail.com>
2019-12-25 17:47:48 +0100

    Include statusbar folder in dmenu prompt

--- .scripts/dmenu_configs/dmenu_configs.sh
+++ .scripts/dmenu_configs/dmenu_configs.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+shopt -s expand_aliases
+alias dmenu='dmenu -fn "UbuntuMono Nerd Font:size=10" -nb "#222222" -nf "#bbbbbb" -sb "#006666" -sf "#eeeeee"'
 
 pathtoconfiglist=$HOME/.scripts/dmenu_configs/config_files.txt
 
-query=$(awk '{print $1}' $pathtoconfiglist | dmenu)
+menulist="$(awk '{print $1}' $pathtoconfiglist)\nstatusbar"
+query=$(echo -e "$menulist" | dmenu -p "What do you want to configure?")
 
 if [ "$(echo $query)" ] && [ "$(awk "\$1~/$query/" $pathtoconfiglist)" ]; then
     filename=$(awk "/$query/ {print \$2;exit}" $pathtoconfiglist)
     st -e vim "$filename"
+elif [ "$query" = "statusbar" ]; then
+    files="$(ls $HOME/.scripts/statusbar/)"
+    file="$(echo "$files" | dmenu -p "Which part of the statusbar?")"
+    if [ "$(echo $file)" ] && [ "$(echo "$files" | awk "\$1~/$file/")" ]; then
+        st -e vim "$HOME/.scripts/statusbar/$file"
+    fi
 fi
