01220c46401c784eaee012e97297b13686bc33d4
shahzebs <shahzeb.syed96@gmail.com>
2020-01-09 12:38:57 +0100

    Delete .screenlayout/

--- .screenlayout/fysikkland_threemonitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-0 --mode 1680x1050 --pos 2704x230 --rotate normal --output DisplayPort-0 --primary --mode 1680x1050 --pos 1024x230 --rotate normal --output DVI-0 --mode 1280x1024 --pos 0x0 --rotate right
