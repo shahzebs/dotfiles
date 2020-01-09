9b6cea7008b9b3ce6b98ba14a176e06241f1f943
shahzebs <shahzeb.syed96@gmail.com>
2020-01-09 11:06:40 +0100

    Ignore dropbox error messages

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# $HOME/.screenlayout/fysikkland_threemonitor.sh &
-
 $HOME/.scripts/local/localcommands > /dev/null 2>&1 &
 
 $HOME/.fehbg &
@@ -12,7 +10,7 @@ fi
 
 compton > /dev/null 2>&1 &
 
-dropbox start > /dev/null &
+dropbox start > /dev/null 2>&1 &
 
 dwmblocks > /dev/null 2>&1 &
 
