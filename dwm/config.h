675d20d8af22c7735bd92efb244d6fb5b1ba65b6
shahzebs <shahzeb.syed96@gmail.com>
2020-01-25 12:02:39 +0100

    Apply movestack patch to dwm

--- dwm/config.h
+++ dwm/config.h
@@ -68,6 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,             XK_d,      spawn,          {.v = dmenucmd } },
@@ -104,6 +105,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,            XK_j,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,            XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
     { MODKEY,                       XK_z,      incrogaps,      {.i = +3 } },
 	{ MODKEY|ShiftMask,             XK_z,      incrogaps,      {.i = -3 } },
