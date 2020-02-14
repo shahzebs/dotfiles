c5bb2cefcde0ea4d081e01b7a0e5231d1490d3cc
shahzebs <shahzeb.syed96@gmail.com>
2020-02-15 00:44:59 +0100

    DWM selected window border colored change to white (#ffffff)

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#458588";  // #005577
-static const char col_bordersel[]   = "#38b33f";  // Green
+static const char col_bordersel[]   = "#ffffff";  // #38b33f
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, "#000000" },
