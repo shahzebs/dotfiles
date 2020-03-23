9b8e85d1e8e09f638689d95c57b570adc6ae97c4
shahzebs <shahzeb.syed96@gmail.com>
2020-03-23 13:25:02 +0100

    Configure fonts to better unicode coverage

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" }; 
-static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" }; 
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
