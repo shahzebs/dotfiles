fca374daaf4171d7571e705099d222d59fdbc6ee
shahzebs <shahzeb.syed96@gmail.com>
2020-03-25 16:55:46 +0100

    Edit all scripts using dmenu to use -i option for case-insensitive matching

--- .scripts/dmenu_configs/dmenu_configs.sh
+++ .scripts/dmenu_configs/dmenu_configs.sh
@@ -3,14 +3,14 @@
 pathtoconfiglist=$HOME/.scripts/dmenu_configs/config_files.txt
 
 menulist="$(awk '{print $1}' $pathtoconfiglist)\nstatusbar"
-query=$(echo -e "$menulist" | dmenu -p "What do you want to configure?")
+query=$(echo -e "$menulist" | dmenu -i -p "What do you want to configure?")
 
 if [ "$(echo $query)" ] && [ "$(awk "\$1~/$query/" $pathtoconfiglist)" ]; then
     filename=$(awk "/^$query\s/ {print \$2;exit}" $pathtoconfiglist)
     st -e vim "$filename"
 elif [ "$query" = "statusbar" ]; then
     files="$(ls $HOME/.scripts/statusbar/)"
-    file="$(echo "$files" | dmenu -p "Which part of the statusbar?")"
+    file="$(echo "$files" | dmenu -i -p "Which part of the statusbar?")"
     if [ "$(echo $file)" ] && [ "$(echo "$files" | awk "\$1~/$file/")" ]; then
         st -e vim "$HOME/.scripts/statusbar/$file"
     fi
