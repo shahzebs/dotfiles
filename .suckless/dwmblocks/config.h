e56c44e657d282224c74d3a0d7f890cb7ac48572
shahzebs <shahzeb.syed96@gmail.com>
2020-04-01 18:55:50 +0200

    Ignore all error messages in dwmblocks

--- .suckless/dwmblocks/config.h
+++ .suckless/dwmblocks/config.h
@@ -1,18 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {" ", "playback", 15, 3},
-    {"", "weather", 900, 0},
-    {"", "dropbx", 10, 0},
-    {"", "cpuload", 3, 0},
-    {"", "cputemp", 3, 0},
-    {"", "memory", 5, 0},
-    {"", "wifi", 10, 2},
-    {"", "volume", 60, 1},
-    {"", "iodevices", 10, 1},
-    {"", "brightness", 0, 4},
-    {"", "battery BAT0", 15, 0},
-	{"", "datetime", 30, 0},
+    {" ", "playback 2>/dev/null", 15, 3},
+    {"", "weather 2>/dev/null", 900, 0},
+    {"", "dropbx 2>/dev/null", 10, 0},
+    {"", "cpuload 2>/dev/null", 3, 0},
+    {"", "cputemp 2>/dev/null", 3, 0},
+    {"", "memory 2>/dev/null", 5, 0},
+    {"", "wifi 2>/dev/null", 10, 2},
+    {"", "volume 2>/dev/null", 60, 1},
+    {"", "iodevices 2>/dev/null", 10, 1},
+    {"", "brightness 2>/dev/null", 0, 4},
+    {"", "battery BAT0 2>/dev/null", 15, 0},
+	{"", "datetime 2>/dev/null", 30, 0},
 
 };
 
