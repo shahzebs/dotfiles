40c862b51a245c23fbf911b5572dfe684f7c8dbd
shahzebs <shahzeb.syed96@gmail.com>
2020-06-19 14:20:54 +0200

    Revert "Apply vanitygaps patch to dwm"
    This reverts commit 57b97f04d69482dfbe7b84222208a5791624dd07.

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -3,11 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" }; 
@@ -109,12 +104,6 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
     { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-    { MODKEY,                       XK_z,      incrogaps,      {.i = +3 } },
-	{ MODKEY|ShiftMask,             XK_z,      incrogaps,      {.i = -3 } },
-	{ MODKEY,                       XK_s,      incrigaps,      {.i = +3 } },
-	{ MODKEY|ShiftMask,             XK_s,      incrigaps,      {.i = -3 } },
-	{ MODKEY|ShiftMask,             XK_t,      togglegaps,     {0} },
-	{ MODKEY|ShiftMask,             XK_d,      defaultgaps,    {0} },
 };
 
 /* button definitions */
