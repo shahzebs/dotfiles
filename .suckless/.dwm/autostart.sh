7cf63a56d08cbacab3f73abeec6a4efc05508a79
Shahzeb Syed <shahzeb.syed@klp.no>
2024-11-18 12:44:08 +0100

    dwm: run startup commands only if system not running in VM

--- .suckless/.dwm/autostart.sh
+++ .suckless/.dwm/autostart.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+IS_VM=$(systemd-detect-virt --vm >/dev/null 2>&1 && echo true || echo false)
+
 # daemon for notifications like notify-send
-/usr/lib/notification-daemon*/notification-daemon &
+[ "$IS_VM" = false ] && /usr/lib/notification-daemon*/notification-daemon &
 
 # run commands specific to the host machine
 localcommands > /dev/null 2>&1 &
 
 # turn off auto screen dimming
-xset s off dpms 3600 7200 0 &
+[ "$IS_VM" = false ] && xset s off dpms 3600 7200 0 &
 
 # simple X hotkey daemon for keybindings
 sxhkd -m 1 > /dev/null 2>&1 &
 
 # compositor
-picom --no-fading-openclose > /dev/null 2>&1 &
+[ "$IS_VM" = false ] && picom --no-fading-openclose > /dev/null 2>&1 &
 
 # statusbar
 dwmblocks > /dev/null 2>&1 &
@@ -33,5 +35,4 @@ setxkbmap -option altwin:menu_win
 clipmenud &
 
 # turn on bluetooth controller
-bluetoothctl power on > /dev/null 2>&1 &
-
+[ "$IS_VM" = false ] && bluetoothctl power on > /dev/null 2>&1 &
